9be9eb94d97b551bc2f481607271c6bb74a47ce5
zeroed <zeroed@posteo.net>
2019-06-17 19:53:33 +0200

    Add tests to cover the ConSet API and the Iterator traits
    This set of tests is strongly based on the tests in ConMap.

--- src/set.rs
+++ src/set.rs
@@ -236,3 +236,269 @@ where
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use crossbeam_utils::thread;
+
+    use super::*;
+    use crate::raw::tests::NoHasher;
+    use crate::raw::LEVEL_CELLS;
+
+    const TEST_THREADS: usize = 4;
+    const TEST_BATCH: usize = 10000;
+    const TEST_BATCH_SMALL: usize = 100;
+    const TEST_REP: usize = 20;
+
+    #[test]
+    fn create_destroy() {
+        let set: ConSet<String> = ConSet::new();
+        drop(set);
+    }
+
+    #[test]
+    fn lookup_empty() {
+        let set: ConSet<String> = ConSet::new();
+        assert!(set.get("hello").is_none());
+    }
+
+    #[test]
+    fn insert_lookup() {
+        let set = ConSet::new();
+        assert!(set.insert("hello").is_none());
+        assert!(set.get("world").is_none());
+        let found = set.get("hello").unwrap();
+        assert_eq!("hello", found);
+    }
+
+    // Insert a lot of things, to make sure we have multiple levels.
+    #[test]
+    fn insert_many() {
+        let set = ConSet::new();
+        for i in 0..TEST_BATCH * LEVEL_CELLS {
+            assert!(set.insert(i).is_none());
+        }
+
+        for i in 0..TEST_BATCH * LEVEL_CELLS {
+            assert_eq!(i, set.get(&i).unwrap());
+        }
+    }
+
+    #[test]
+    fn par_insert_many() {
+        for _ in 0..TEST_REP {
+            let set: ConSet<usize> = ConSet::new();
+            thread::scope(|s| {
+                for t in 0..TEST_THREADS {
+                    let set = &set;
+                    s.spawn(move |_| {
+                        for i in 0..TEST_BATCH {
+                            let num = t * TEST_BATCH + i;
+                            assert!(set.insert(num).is_none());
+                        }
+                    });
+                }
+            })
+            .unwrap();
+
+            for i in 0..TEST_BATCH * TEST_THREADS {
+                assert_eq!(set.get(&i).unwrap(), i);
+            }
+        }
+    }
+
+    #[test]
+    fn par_get_many() {
+        for _ in 0..TEST_REP {
+            let set = ConSet::new();
+            for i in 0..TEST_BATCH * TEST_THREADS {
+                assert!(set.insert(i).is_none());
+            }
+            thread::scope(|s| {
+                for t in 0..TEST_THREADS {
+                    let set = &set;
+                    s.spawn(move |_| {
+                        for i in 0..TEST_BATCH {
+                            let num = t * TEST_BATCH + i;
+                            assert_eq!(set.get(&num).unwrap(), num);
+                        }
+                    });
+                }
+            })
+            .unwrap();
+        }
+    }
+
+    #[test]
+    fn no_collisions() {
+        let set = ConSet::with_hasher(NoHasher);
+        // While their hash is the same under the hasher, they don't kick each other out.
+        for i in 0..TEST_BATCH_SMALL {
+            assert!(set.insert(i).is_none());
+        }
+        // And all are present.
+        for i in 0..TEST_BATCH_SMALL {
+            assert_eq!(i, set.get(&i).unwrap());
+        }
+        // No key kicks another one out.
+        for i in 0..TEST_BATCH_SMALL {
+            assert_eq!(i, set.insert(i).unwrap());
+        }
+    }
+
+    #[test]
+    fn simple_remove() {
+        let set = ConSet::new();
+        assert!(set.remove(&42).is_none());
+        assert!(set.insert(42).is_none());
+        assert_eq!(42, set.get(&42).unwrap());
+        assert_eq!(42, set.remove(&42).unwrap());
+        assert!(set.get(&42).is_none());
+        assert!(set.is_empty());
+        assert!(set.remove(&42).is_none());
+        assert!(set.is_empty());
+    }
+
+    fn remove_many_inner<H: BuildHasher>(mut set: ConSet<usize, H>, len: usize) {
+        for i in 0..len {
+            assert!(set.insert(i).is_none());
+        }
+        for i in 0..len {
+            assert_eq!(i, set.get(&i).unwrap());
+            assert_eq!(i, set.remove(&i).unwrap());
+            assert!(set.get(&i).is_none());
+            set.raw.assert_pruned();
+        }
+
+        assert!(set.is_empty());
+    }
+
+    #[test]
+    fn remove_many() {
+        remove_many_inner(ConSet::new(), TEST_BATCH);
+    }
+
+    #[test]
+    fn remove_many_collision() {
+        remove_many_inner(ConSet::with_hasher(NoHasher), TEST_BATCH_SMALL);
+    }
+
+    #[test]
+    fn collision_remove_one_left() {
+        let mut set = ConSet::with_hasher(NoHasher);
+        set.insert(1);
+        set.insert(2);
+
+        set.raw.assert_pruned();
+
+        assert!(set.remove(&2).is_some());
+        set.raw.assert_pruned();
+
+        assert!(set.remove(&1).is_some());
+
+        set.raw.assert_pruned();
+        assert!(set.is_empty());
+    }
+
+    #[test]
+    fn collision_remove_one_left_with_str() {
+        let mut set = ConSet::with_hasher(NoHasher);
+        set.insert("hello");
+        set.insert("world");
+
+        set.raw.assert_pruned();
+
+        assert!(set.remove("world").is_some());
+        set.raw.assert_pruned();
+
+        assert!(set.remove("hello").is_some());
+
+        set.raw.assert_pruned();
+        assert!(set.is_empty());
+    }
+
+    #[test]
+    fn remove_par() {
+        let mut set = ConSet::new();
+        for i in 0..TEST_THREADS * TEST_BATCH {
+            set.insert(i);
+        }
+
+        thread::scope(|s| {
+            for t in 0..TEST_THREADS {
+                let set = &set;
+                s.spawn(move |_| {
+                    for i in 0..TEST_BATCH {
+                        let num = t * TEST_BATCH + i;
+                        let val = set.remove(&num).unwrap();
+                        assert_eq!(num, val);
+                        assert_eq!(num, val);
+                    }
+                });
+            }
+        })
+        .unwrap();
+
+        set.raw.assert_pruned();
+        assert!(set.is_empty());
+    }
+
+    fn iter_test_inner<S: BuildHasher>(set: ConSet<usize, S>) {
+        for i in 0..TEST_BATCH_SMALL {
+            assert!(set.insert(i).is_none());
+        }
+
+        let mut extracted = set.iter().collect::<Vec<_>>();
+
+        extracted.sort();
+        let expected = (0..TEST_BATCH_SMALL).into_iter().collect::<Vec<_>>();
+        assert_eq!(expected, extracted);
+    }
+
+    #[test]
+    fn iter() {
+        let set = ConSet::new();
+        iter_test_inner(set);
+    }
+
+    #[test]
+    fn iter_collision() {
+        let set = ConSet::with_hasher(NoHasher);
+        iter_test_inner(set);
+    }
+
+    #[test]
+    fn collect() {
+        let set = (0..TEST_BATCH_SMALL).into_iter().collect::<ConSet<_>>();
+
+        let mut extracted = set.iter().collect::<Vec<_>>();
+        extracted.sort();
+        let expected = (0..TEST_BATCH_SMALL).into_iter().collect::<Vec<_>>();
+        assert_eq!(expected, extracted);
+    }
+
+    #[test]
+    fn par_extend() {
+        let set = ConSet::new();
+
+        thread::scope(|s| {
+            for t in 0..TEST_THREADS {
+                let mut set = &set;
+                s.spawn(move |_| {
+                    let start = t * TEST_BATCH_SMALL;
+                    let iter = (start..start + TEST_BATCH_SMALL).into_iter();
+                    set.extend(iter);
+                });
+            }
+        })
+        .unwrap();
+
+        let mut extracted = set.iter().collect::<Vec<_>>();
+
+        extracted.sort();
+        let expected = (0..TEST_THREADS * TEST_BATCH_SMALL)
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        assert_eq!(expected, extracted);
+    }
+}
